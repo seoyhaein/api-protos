6ecfbd642c7d56dcb91d433cf1f1184306995390
seoyhaein <seoy@bio-caleb.com>
2025-05-30 19:35:02 +0900

    update

--- gen/go/datablock/ichthys/v1/service/data_block_service_server.go
+++ gen/go/datablock/ichthys/v1/service/data_block_service_server.go
@@ -4,42 +4,48 @@ import (
 	"context"
 	pb "github.com/seoyhaein/api-protos/gen/go/datablock/ichthys/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// 좀더 살펴보자.
-// 문서화 해놓자. 자꾸 잊어버림.
-
-// 1) 최소한의 핸들러 인터페이스 정의
 type DataBlockHandler interface {
-	// 비즈니스 로직에 맞춘 시그니처
 	GetDataBlock(ctx context.Context, updateAt *timestamppb.Timestamp) (*pb.DataBlock, error)
 }
 
-// 2) gRPC 서버 구조체는 핸들러만 보유
 type dataBlockServiceServerImpl struct {
 	pb.UnimplementedDataBlockServiceServer
 	handler DataBlockHandler
 }
 
-// 3) 생성자: 핸들러 인터페이스를 받음
 func NewDataBlockServiceServer(h DataBlockHandler) pb.DataBlockServiceServer {
 	return &dataBlockServiceServerImpl{handler: h}
 }
 
-// 4) gRPC 메서드는 핸들러에 위임
 func (s *dataBlockServiceServerImpl) GetDataBlock(ctx context.Context, in *pb.FetchDataBlockRequest) (*pb.FetchDataBlockResponse, error) {
+	// TODO 위힘 함. 추가적인 설명 달아 놓을 것
 	data, err := s.handler.GetDataBlock(ctx, in.IfModifiedSince)
+
 	if err != nil {
 		return nil, err
 	}
+
+	// 2) 변경 없음(no_update) 케이스
 	if data == nil {
-		return &pb.GetDataBlockResponse{NoUpdate: true}, nil
+		return &pb.FetchDataBlockResponse{
+			Payload: &pb.FetchDataBlockResponse_NoUpdate{
+				NoUpdate: &emptypb.Empty{},
+			},
+		}, nil
 	}
-	return &pb.GetDataBlockResponse{Data: data, NoUpdate: false}, nil
+
+	// 3) 변경된 DataBlock(data_block) 케이스
+	return &pb.FetchDataBlockResponse{
+		Payload: &pb.FetchDataBlockResponse_DataBlock{
+			DataBlock: data,
+		},
+	}, nil
 }
 
-// 5) 서버 등록 헬퍼
 func RegisterDataBlockService(s *grpc.Server, h DataBlockHandler) {
 	pb.RegisterDataBlockServiceServer(s, NewDataBlockServiceServer(h))
 }
