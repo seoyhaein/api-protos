b66535ee424a9bf1126f4360087d9cca4a6eae5b
seoyhaein <seoy@bio-caleb.com>
2025-05-30 04:36:54 +0000

    bug fixed

--- generate-proto.sh
+++ generate-proto.sh
@@ -11,12 +11,12 @@ echo "🔄 buf generate"
 # 서비스가 생길때마다 여기에 추가 해야함
 # 1) datablock 서비스만
 buf generate . \
-  --template buf.gen.yaml \
+  --template protos/datablock/ichthys/v1/buf.gen.datablock.yaml \
   --path protos/datablock/ichthys/v1/datablock_service.proto
 
 # 2) syncfolders 서비스만
 buf generate . \
-  --template buf.gen.yaml \
+  --template protos/syncfolders/ichthys/v1/buf.gen.syncfolders.yaml \
   --path protos/syncfolders/ichthys/v1/syncfolders_service.proto
 
 echo "✅ 완료!"
